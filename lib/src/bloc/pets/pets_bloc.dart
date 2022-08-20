8fbe96c8816b43cc5328e0ca9a87eadf8c266fa6
Assi <ali@algooru.com>
2022-08-20 08:28:22 +0300

    reorganize files

--- lib/src/bloc/pets/pets_bloc.dart
+++ lib/src/bloc/pets/pets_bloc.dart
@@ -1,5 +1,5 @@
-import 'package:dogs_and_cats/src/pets/pets_events.dart';
-import 'package:dogs_and_cats/src/pets/pets_state.dart';
+import 'package:dogs_and_cats/src/bloc/pets/pets_events.dart';
+import 'package:dogs_and_cats/src/bloc/pets/pets_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PetsBloc extends Bloc<PetsEvents, PetsState> {
