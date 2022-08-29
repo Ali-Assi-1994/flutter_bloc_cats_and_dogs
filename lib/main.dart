c8b1e9bb2941fa97124dc13fe856dc7057e10833
Assi <ali@algooru.com>
2022-08-30 00:19:16 +0300

    firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
+import 'package:dogs_and_cats/firebase_options.dart';
 import 'package:dogs_and_cats/generated/l10n.dart';
 import 'package:dogs_and_cats/src/bloc/cats/cats_bloc.dart';
 import 'package:dogs_and_cats/src/bloc/dogs/dogs_bloc.dart';
 import 'package:dogs_and_cats/src/bloc/home/home_bloc.dart';
 import 'package:dogs_and_cats/src/data_layer/pets_repository/pets_repository.dart';
 import 'package:dogs_and_cats/src/ui/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+Future<void> main() async {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
