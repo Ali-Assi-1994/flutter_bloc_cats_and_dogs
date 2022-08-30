ea9177e64f2a05c38169da26075172291a135f9c
Assi <ali@algooru.com>
2022-08-30 23:06:40 +0300

    loading dialog

--- lib/src/utils/app_secrets.dart
+++ lib/src/utils/app_secrets.dart
@@ -1,4 +1,4 @@
-class Constants {
+class AppSecrets {
   static const String dogApiKey = 'live_JIZpzoucExGTqmaKcf5dRDQyMHa5Jnp1JrTsSaaxgNVhwGlX5X4ub7KV9geQRTMq';
   static const String catApiKey = '226fca60-9fc0-4df9-baf6-5b060234013b';
 
